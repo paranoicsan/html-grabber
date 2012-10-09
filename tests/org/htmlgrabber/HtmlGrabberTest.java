56d8085afef14279453b974806101d6820ceb291
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-10-09 22:00:23 +0300

    Подключены библиотеки для парсинга DOM-структуры скаченных для анализа страниц.

---
+++ tests/org/htmlgrabber/HtmlGrabberTest.java
@@ -0,0 +1,24 @@
+package org.htmlgrabber;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * org.htmlgrabber
+ * User: sanych
+ * Date: 09.10.12
+ * Time: 21:50
+ */
+public class HtmlGrabberTest {
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+
+
+}
