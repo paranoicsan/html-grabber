4ec5ec2b8bbdb4fe937aa8708784e523540252b0
Alexander Kuznetsov <paranoic.san@gmail.com>
2012-10-09 22:46:59 +0300

    Добавлены тесты для парсинга загруженной страницы.

--- src/org/htmlgrabber/HtmlGrabber.java
+++ src/org/htmlgrabber/HtmlGrabber.java
@@ -1,5 +1,8 @@
 package org.htmlgrabber;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 /**
  * org.htmlgrabber
  * User: sanych
@@ -7,6 +10,37 @@ package org.htmlgrabber;
  * Time: 20:46
  */
 public class HtmlGrabber {
+
+    /**
+     * URL страницы, которую надо загрузить
+     */
+    private String url;
+    /**
+     * Загруженная и обработанная страница
+     */
+    private Document document;
+
+    /**
+     * Конструктор объекта для граббинга страниц
+     * @param url Адрес страницы, которую надо загрузить для анализа
+     */
+    public HtmlGrabber(String url) {
+        this.url = url;
+    }
+
+    /**
+     * Выполняет загрузку старницы
+     */
+    public void parsePage() throws Exception {
+        document = Jsoup.connect(url)
+                .userAgent("Mozilla")
+                .timeout(3000)
+                .get();
+    }
+
+    public Document getDocument() {
+        return document;
+    }
 }
 
 
--- tests/org/htmlgrabber/HtmlGrabberTest.java
+++ tests/org/htmlgrabber/HtmlGrabberTest.java
@@ -1,7 +1,9 @@
 package org.htmlgrabber;
 
+import junit.framework.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 /**
  * org.htmlgrabber
@@ -10,9 +12,13 @@ import org.testng.annotations.BeforeMethod;
  * Time: 21:50
  */
 public class HtmlGrabberTest {
+
+    private HtmlGrabber grabber;
+    private static final String TEST_URL = "http://www.google.com";
+
     @BeforeMethod
     public void setUp() throws Exception {
-
+        grabber = new HtmlGrabber(TEST_URL);
     }
 
     @AfterMethod
@@ -20,5 +26,20 @@ public class HtmlGrabberTest {
 
     }
 
+    @Test
+    public void testDefaultConstruction() {
+        Assert.assertNotNull(grabber);
+    }
+
+    /**
+     *  Должен скачать запросом страницу и пропарсить её DOM-структуру
+     */
+    @Test
+    public void testParsePage() throws Exception {
+        grabber.parsePage();
+        Assert.assertNotNull(grabber.getDocument());
+        String title = "Google";
+        Assert.assertEquals(title, grabber.getDocument().title());
+    }
 
 }
